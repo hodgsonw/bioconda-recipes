dab3c2c843e7f1e4e0338ec99bdf3d2f0471056b
hodgsonw <wilson.hodgson@canada.ca>
2018-10-10 14:06:49 -0400

    Initial working copy of virtool package

---
+++ recipes/virtool-2.3.2-0_recipe_#13514/build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+PKG_NAME=virtool
+PKG_VERSION=2.3.2
+PKG_BUILDNUM=0
+
+INSTALL=${PREFIX}/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
+
+#build client
+
+#pip install -r ${SRC_DIR}/requirements.txt
+
+npm install -g yarn
+npm install -g webpack@v3.11.0
+
+cd ${SRC_DIR}
+cd client
+yarn install
+webpack --config webpack.production.config.babel.js
+
+
+#create install folder
+mkdir -p $INSTALL
+
+#move from sourcedir to install location
+cp -r ${SRC_DIR}/* $INSTALL
+
+#ensure executables are available in the environment's bin folder
+ln -s ${INSTALL}/run.py ${PREFIX}/bin/run.py
+
+chmod 0755 ${PREFIX}/run.py
