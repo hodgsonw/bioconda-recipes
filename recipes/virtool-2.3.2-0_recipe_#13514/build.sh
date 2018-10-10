65ded6b57e4a8c4075cc5428a4fb0683480477e3
hodgsonw <wilson.hodgson@canada.ca>
2018-10-10 14:07:43 -0400

    cleaned up meta.yaml, added mongo config to build script, added bash wrapper for virtool

--- recipes/virtool-2.3.2-0_recipe_#13514/build.sh
+++ recipes/virtool-2.3.2-0_recipe_#13514/build.sh
@@ -7,21 +7,45 @@ PKG_BUILDNUM=0
 INSTALL=${PREFIX}/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 
 #build client
-if [ -e ~/.npm ] ; then
-  rm -rf ~/.npm ;
-fi
-
-npm install -g yarn
-npm install -g webpack@v3.11.0
+npm --production --loglevel warn install -g yarn webpack@v3.11.0
 
 cd ${SRC_DIR}
 cd client
 yarn install
 webpack --config webpack.production.config.babel.js
 
+#remove build dependencies
+npm uninstall -g webpack yarn 
 
 #create install folder
 mkdir -p $INSTALL
+
+#configure mongodb for virtool
+mkdir -p $INSTALL/mongo
+mkdir -p $INSTALL/virtooldb
+
+#conda removes empty folders from packages. this keeps the virtooldb folder present for mongo
+touch $INSTALL/virtooldb/.deleteMe
+
+
+#set up mongodb config
+cat << EOF >$INSTALL/mongo/mongo.conf
+processManagement:
+   fork: true
+   pidFilePath: ./virtooldb.pid
+net:
+   bindIp: localhost
+   port: 27017
+storage:
+   dbPath: ../virtooldb/
+systemLog:
+   destination: file
+   path: ./virtooldb.log
+   logAppend: true
+storage:
+   journal:
+       enabled: true
+EOF
 
 #move from sourcedir to install location
 cp -r ${SRC_DIR}/* $INSTALL
@@ -29,4 +53,3 @@ cp -r ${SRC_DIR}/* $INSTALL
 #ensure executables are available in the environment's bin folder
 ln -s ${INSTALL}/run.py ${PREFIX}/bin/run.py
 
-chmod 0755 ${PREFIX}/bin/run.py
